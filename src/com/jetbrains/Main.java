2cbf7edef27bb397760aa6a67d38d62954ea5d36
Dmitry Jemerov <yole@jetbrains.com>
2019-09-19 12:10:37 +0200

    Initial commit

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,12 @@
+package com.jetbrains;
+
+/**
+ * @author yole
+ */
+public class Main {
+  public static void main(String[] args) {
+    for (String c : args) {
+      System.out.println(c);
+    }
+  }
+}
